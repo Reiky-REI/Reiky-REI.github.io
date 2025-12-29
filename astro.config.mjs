c4cb7591b2610fc0fe91623594f024ff00f7543f
__reiky__ <32432650+Reiky-REI@users.noreply.github.com>
2025-12-29 19:12:22 +0000

    init the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,9 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	//site: "https://fuwari.vercel.app/",
+	site: "https://reiky-rei.github.io/",
+	base: "/my-astro-blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
